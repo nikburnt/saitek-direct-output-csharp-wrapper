bdc3c1e91721b1080d1a13c3a863949e72489940
daspisch <daspisch@gmail.com>
2014-12-16 20:16:33 +0300

    Studio initial commit with library placeholder and unit test example

---
+++ WrapperTest/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using DirectInputCSharpWrapper;
+
+namespace WrapperTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DirectInput directInput = new DirectInput();
+            Assert.AreEqual("ololo", directInput.Test());
+        }
+    }
+}
