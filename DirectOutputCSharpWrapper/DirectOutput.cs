63fe5897a18f9b5af6ee0a32f7bd09ddd614c968
Nikita Gorelikov <daspisch@gmail.com>
2014-12-18 10:52:46 +0300

    Added loading libpath from registry Solution reworked

---
+++ DirectOutputCSharpWrapper/DirectOutput.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.Win32;
+
+namespace DirectOutputCSharpWrapper
+{
+    public class DirectOutput
+    {
+        private const String directOutputKey = "SOFTWARE\\Saitek\\DirectOutput";
+
+        public bool isActive
+        {
+            get
+            {
+                return libraryPath != null;
+            }
+        }
+
+        String libraryPath = null;
+
+        public DirectOutput()
+        {
+            RegistryKey key = Registry.LocalMachine.OpenSubKey(directOutputKey);
+            if (key == null)
+            {
+                // No Key - No Game)
+                return;
+            }
+
+            object value = key.GetValue("DirectOutput");
+            if (value != null && value is String)
+            {
+                libraryPath = (String)value;
+            }
+        }
+    }
+}
---
+++ WrapperTest/DirectOutputTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using DirectOutputCSharpWrapper;
+
+namespace WrapperTest
+{
+    [TestClass]
+    public class DirectOutputTest
+    {
+        [TestMethod]
+        public void IsLibraryFounded()
+        {
+            DirectOutput directInput = new DirectOutput();
+            Assert.AreEqual(true, directInput.isActive);
+        }
+    }
+}
