f96a22dd70133ee758dd76dd990cff0f5f20a50a
daspisch <daspisch@gmail.com>
2014-12-23 12:11:14 +0300

    Added Initialize/Deinitialize methods implementation Added HResultException Class Tests updated

--- DirectOutputCSharpWrapper/DirectOutput.cs
+++ DirectOutputCSharpWrapper/DirectOutput.cs
@@ -1,9 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Text;
+
 using Microsoft.Win32;
 
-using System.IO;
-using System.Text;
-using System.Runtime.InteropServices;
+using HResult = System.Int32;
 
 namespace DirectOutputCSharpWrapper {
 
@@ -13,6 +16,19 @@ namespace DirectOutputCSharpWrapper {
 
 	public class RegistryValueNotFound : Exception {
 		public RegistryValueNotFound() : base(@"DirectOutput value in key HKEY_LOCAL_MACHINE\SOFTWARE\Saitek\DirectOutput not found.") {}
+	}
+
+	public class HResultException : Exception {
+
+		public const HResult S_OK = 0x00000000;
+		public const HResult E_OUTOFMEMORY = unchecked((HResult)0x8007000E);
+		public const HResult E_INVALIDARG = unchecked((HResult)0x80070057);
+		public const HResult E_HANDLE = unchecked((HResult)0x80070006);
+
+		public HResultException(HResult hResult, Dictionary<HResult, String> errorsMap)
+			: base(errorsMap[hResult]) {
+			HResult = hResult;
+		}
 	}
 
 	public class DirectOutput {
@@ -31,32 +47,32 @@ namespace DirectOutputCSharpWrapper {
 		public delegate void PageCallback(IntPtr hDevice, bool bActivated, IntPtr pvContext);
 
 		// Library functions
-		private delegate Int32 Initialize([MarshalAsAttribute(UnmanagedType.LPWStr)] string appName);
-		private delegate Int32 Deinitialize();
-		private delegate Int32 RegisterDeviceCallback([MarshalAs(UnmanagedType.FunctionPtr)]DeviceCallback callback, IntPtr target);
-		private delegate Int32 Enumerate([MarshalAs(UnmanagedType.FunctionPtr)]EnumerateCallback callback, IntPtr target);
-		private delegate Int32 GetDeviceType(IntPtr hDevice, out Guid pGuidType);
-		private delegate Int32 GetDeviceInstance(IntPtr hDevice, out Guid pGuidInstance);
-		private delegate Int32 SetProfile(IntPtr hDevice, Int32 fileNameLength, [MarshalAsAttribute(UnmanagedType.LPWStr)] string filename);
-		private delegate Int32 RegisterSoftButtonCallback(IntPtr hDevice, [MarshalAs(UnmanagedType.FunctionPtr)]SoftButtonCallback callback, IntPtr target);
-		private delegate Int32 RegisterPageCallback(IntPtr hDevice, [MarshalAs(UnmanagedType.FunctionPtr)]PageCallback callback, IntPtr target);
-		private delegate Int32 AddPage(IntPtr hDevice, Int32 dwPage, [MarshalAsAttribute(UnmanagedType.LPWStr)] string wszName, Int32 dwFlags);
-		private delegate Int32 RemovePage(IntPtr hDevice, Int32 dwPage);
-		private delegate Int32 SetLed(IntPtr hDevice, Int32 dwPage, Int32 dwIndex, Int32 dwValue);
-		private delegate Int32 SetString(IntPtr hDevice, Int32 dwPage, Int32 dwIndex, Int32 cchValue, [MarshalAsAttribute(UnmanagedType.LPWStr)] string wszValue);
-		private delegate Int32 SetImage(IntPtr hDevice, Int32 dwPage, Int32 dwIndex, Int32 cbValue, byte[] pbValue);
-		private delegate Int32 SetImageFromFile(IntPtr hDevice, Int32 dwPage, Int32 dwIndex, Int32 fileNameLength, [MarshalAsAttribute(UnmanagedType.LPWStr)] string filename);
-		private delegate Int32 StartServer(IntPtr hDevice, Int32 fileNameLength, [MarshalAsAttribute(UnmanagedType.LPWStr)] string filename, out IntPtr pdwServerId, out SRequestStatus psStatus);
-		private delegate Int32 CloseServer(IntPtr hDevice, Int32 dwServerId, out SRequestStatus psStatus);
-		private delegate Int32 SendServerMsg(IntPtr hDevice, Int32 dwServerId, Int32 dwRequest, Int32 dwPage, Int32 cbIn, IntPtr pvIn, Int32 cbOut, out IntPtr pvOut, out SRequestStatus psStatus);
-		private delegate Int32 SendServerFile(IntPtr hDevice, Int32 dwServerId, Int32 dwRequest, Int32 dwPage, Int32 cbInHdr, IntPtr pvInHdr, Int32 fileNameLength, [MarshalAsAttribute(UnmanagedType.LPWStr)] string filename, Int32 cbOut, out IntPtr pvOut, out SRequestStatus psStatus);
-		private delegate Int32 SaveFile(IntPtr hDevice, Int32 dwPage, Int32 dwFile, Int32 fileNameLength, [MarshalAsAttribute(UnmanagedType.LPWStr)] string filename, out SRequestStatus psStatus);
-		private delegate Int32 DisplayFile(IntPtr hDevice, Int32 dwPage, Int32 dwIndex, Int32 dwFile, out SRequestStatus psStatus);
-		private delegate Int32 DeleteFile(IntPtr hDevice, Int32 dwPage, Int32 dwFile, out SRequestStatus psStatus);
+		private delegate HResult DirectOutput_Initialize([MarshalAsAttribute(UnmanagedType.LPWStr)] String appName);
+		private delegate HResult DirectOutput_Deinitialize();
+		private delegate HResult RegisterDeviceCallback([MarshalAs(UnmanagedType.FunctionPtr)]DeviceCallback callback, IntPtr target);
+		private delegate HResult Enumerate([MarshalAs(UnmanagedType.FunctionPtr)]EnumerateCallback callback, IntPtr target);
+		private delegate HResult GetDeviceType(IntPtr hDevice, out Guid pGuidType);
+		private delegate HResult GetDeviceInstance(IntPtr hDevice, out Guid pGuidInstance);
+		private delegate HResult SetProfile(IntPtr hDevice, Int32 fileNameLength, [MarshalAsAttribute(UnmanagedType.LPWStr)] String filename);
+		private delegate HResult RegisterSoftButtonCallback(IntPtr hDevice, [MarshalAs(UnmanagedType.FunctionPtr)]SoftButtonCallback callback, IntPtr target);
+		private delegate HResult RegisterPageCallback(IntPtr hDevice, [MarshalAs(UnmanagedType.FunctionPtr)]PageCallback callback, IntPtr target);
+		private delegate HResult AddPage(IntPtr hDevice, Int32 dwPage, [MarshalAsAttribute(UnmanagedType.LPWStr)] String wszName, Int32 dwFlags);
+		private delegate HResult RemovePage(IntPtr hDevice, Int32 dwPage);
+		private delegate HResult SetLed(IntPtr hDevice, Int32 dwPage, Int32 dwIndex, Int32 dwValue);
+		private delegate HResult SetString(IntPtr hDevice, Int32 dwPage, Int32 dwIndex, Int32 cchValue, [MarshalAsAttribute(UnmanagedType.LPWStr)] String wszValue);
+		private delegate HResult SetImage(IntPtr hDevice, Int32 dwPage, Int32 dwIndex, Int32 cbValue, byte[] pbValue);
+		private delegate HResult SetImageFromFile(IntPtr hDevice, Int32 dwPage, Int32 dwIndex, Int32 fileNameLength, [MarshalAsAttribute(UnmanagedType.LPWStr)] String filename);
+		private delegate HResult StartServer(IntPtr hDevice, Int32 fileNameLength, [MarshalAsAttribute(UnmanagedType.LPWStr)] String filename, out IntPtr pdwServerId, out SRequestStatus psStatus);
+		private delegate HResult CloseServer(IntPtr hDevice, Int32 dwServerId, out SRequestStatus psStatus);
+		private delegate HResult SendServerMsg(IntPtr hDevice, Int32 dwServerId, Int32 dwRequest, Int32 dwPage, Int32 cbIn, IntPtr pvIn, Int32 cbOut, out IntPtr pvOut, out SRequestStatus psStatus);
+		private delegate HResult SendServerFile(IntPtr hDevice, Int32 dwServerId, Int32 dwRequest, Int32 dwPage, Int32 cbInHdr, IntPtr pvInHdr, Int32 fileNameLength, [MarshalAsAttribute(UnmanagedType.LPWStr)] String filename, Int32 cbOut, out IntPtr pvOut, out SRequestStatus psStatus);
+		private delegate HResult SaveFile(IntPtr hDevice, Int32 dwPage, Int32 dwFile, Int32 fileNameLength, [MarshalAsAttribute(UnmanagedType.LPWStr)] String filename, out SRequestStatus psStatus);
+		private delegate HResult DisplayFile(IntPtr hDevice, Int32 dwPage, Int32 dwIndex, Int32 dwFile, out SRequestStatus psStatus);
+		private delegate HResult DeleteFile(IntPtr hDevice, Int32 dwPage, Int32 dwFile, out SRequestStatus psStatus);
 		
 		// Functions placeholders
-		private Initialize initialize;
-		private Deinitialize deinitialize;
+		private DirectOutput_Initialize initialize;
+		private DirectOutput_Deinitialize deinitialize;
 		private RegisterDeviceCallback registerDeviceCallback;
 		private Enumerate enumerate;
 		private GetDeviceType getDeviceType;
@@ -83,24 +99,47 @@ namespace DirectOutputCSharpWrapper {
 		private GCHandle thisHandle;
 		private UInt32 hModule;
 
-		public DirectOutput() {
+		/// <summary>
+		/// Creates DirectOutput wrapper
+		/// </summary>
+		/// <param name="libPath">Path to DirectOutput.dll</param>
+		/// <exception cref="RegistryKeyNotFound">HKEY_LOCAL_MACHINE\SOFTWARE\Saitek\DirectOutput key not found. Usualy, this mean what Saitek Drivers not installed properly.</exception>
+		/// <exception cref="RegistryValueNotFound">DirectOutput value in key HKEY_LOCAL_MACHINE\SOFTWARE\Saitek\DirectOutput key not found. Usualy, this mean what Saitek Drivers not installed properly.</exception>
+		/// <exception cref="DllHelper.OutOfMemoryException">The system is out of memory or resources.</exception>
+		/// <exception cref="DllHelper.BadFormatException">The target file is invalid.</exception>
+		/// <exception cref="DllHelper.FileNotFountException">The specified file was not found.</exception>
+		/// <exception cref="DllHelper.PathNotFountException">The specified path was not found.</exception>
+		/// <exception cref="DllHelper.UnknownException">Unknown exception during library loading.</exception>
+		public DirectOutput(String libPath = null) {
 			thisHandle = GCHandle.Alloc(this);
 
-			RegistryKey key = Registry.LocalMachine.OpenSubKey(directOutputKey);
-			if (key == null) {
-				throw new RegistryKeyNotFound();
+			if (libPath == null) {
+				RegistryKey key = Registry.LocalMachine.OpenSubKey(directOutputKey);
+				if (key == null) {
+					throw new RegistryKeyNotFound();
+				}
+
+				object value = key.GetValue("DirectOutput");
+				if ((value == null) || !(value is String)) {
+					throw new RegistryValueNotFound();
+				}
+
+				libPath = (String)value;
 			}
 
-			object value = key.GetValue("DirectOutput");
-			if ((value == null) || !(value is String)) {
-				throw new RegistryValueNotFound();
-			}
+			hModule = DllHelper.LoadLibrary(libPath);
 
-			hModule = DllHelper.LoadLibrary((String)value);
+			InitializeLibraryFunctions();
+		}
 
-			// Initialize functions
-			initialize = DllHelper.GetFunction<Initialize>(hModule, "DirectOutput_Initialize");
-			deinitialize = DllHelper.GetFunction<Deinitialize>(hModule, "DirectOutput_Deinitialize");
+		~DirectOutput() {
+			DllHelper.FreeLibrary(hModule);
+			thisHandle.Free();
+		}
+
+		private void InitializeLibraryFunctions() {
+			initialize = DllHelper.GetFunction<DirectOutput_Initialize>(hModule, "DirectOutput_Initialize");
+			deinitialize = DllHelper.GetFunction<DirectOutput_Deinitialize>(hModule, "DirectOutput_Deinitialize");
 			registerDeviceCallback = DllHelper.GetFunction<RegisterDeviceCallback>(hModule, "DirectOutput_RegisterDeviceCallback");
 			enumerate = DllHelper.GetFunction<Enumerate>(hModule, "DirectOutput_Enumerate");
 			getDeviceType = DllHelper.GetFunction<GetDeviceType>(hModule, "DirectOutput_GetDeviceType");
@@ -120,12 +159,44 @@ namespace DirectOutputCSharpWrapper {
 			sendServerFile = DllHelper.GetFunction<SendServerFile>(hModule, "DirectOutput_SendServerFile");
 			saveFile = DllHelper.GetFunction<SaveFile>(hModule, "DirectOutput_SaveFile");
 			displayFile = DllHelper.GetFunction<DisplayFile>(hModule, "DirectOutput_DisplayFile");
-			deleteFile = DllHelper.GetFunction<DeleteFile>(hModule, "DirectOutput_DeleteFile");
+			deleteFile = DllHelper.GetFunction<DeleteFile>(hModule, "DirectOutput_DeleteFile");		
 		}
 
-		~DirectOutput() {
-			DllHelper.FreeLibrary(hModule);
-			thisHandle.Free();
+		/// <summary>
+		/// Initialize the DirectOutput library.
+		/// </summary>
+		/// <param name="appName">String that specifies the name of the application. Optional</param>
+		/// <remarks>
+		/// This function must be called before calling any others. Call this function when you want to initialize the DirectOutput library.
+		/// </remarks>
+		/// <exception cref="HResultException"></exception>
+		public void Initialize(String appName = "DirectOutputCSharpWrapper") {
+			HResult retVal = initialize(appName);
+			if (retVal != HResultException.S_OK) {
+				Dictionary<HResult, String> errorsMap = new Dictionary<HResult, String>() {
+					{HResultException.E_OUTOFMEMORY, "There was insufficient memory to complete this call."},
+					{HResultException.E_INVALIDARG, "The argument is invalid."},
+					{HResultException.E_HANDLE, "The DirectOutputManager prcess could not be found."}
+				};
+				throw new HResultException(retVal, errorsMap);
+			}
+		}
+
+		/// <summary>
+		/// Clean up the DirectOutput library
+		/// </summary>
+		/// <remarks>
+		/// This function must be called before termination. Call this function to clean up any resources allocated by <see cref="Initialize"/> .
+		/// </remarks>
+		/// <exception cref="HResultException"></exception>
+		public void Deinitialize() {
+			HResult retVal = deinitialize();
+			if (retVal != HResultException.S_OK) {
+				Dictionary<HResult, String> errorsMap = new Dictionary<HResult, String>() {
+					{HResultException.E_HANDLE, "DirectOutput was not initialized or was already deinitialized."}
+				};
+				throw new HResultException(retVal, errorsMap);
+			}
 		}
 
 	}
--- WrapperTest/DirectOutputTest.cs
+++ WrapperTest/DirectOutputTest.cs
@@ -3,16 +3,31 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 using DirectOutputCSharpWrapper;
 
-namespace WrapperTest
-{
-    [TestClass]
-    public class DirectOutputTest
-    {
-        [TestMethod]
-        public void IsLibraryFounded()
-        {
-            DirectOutput directInput = new DirectOutput();
-            Assert.AreEqual(true, directInput.isActive);
-        }
-    }
+namespace WrapperTest {
+	[TestClass]
+	public class DirectOutputTest {
+		private DirectOutput directOutput;
+
+		[TestInitialize()]
+		public void Prepare() {
+			directOutput = new DirectOutput(@"c:\Users\daspisch\Downloads\DirectOutput\DirectOutput_x86.dll");
+		}
+
+		[TestCleanup()]
+		public void Cleanup() {
+		}
+
+		[TestMethod]
+		public void Initialize() {
+			directOutput.Initialize();
+			Assert.IsTrue(true);
+		}
+
+		[TestMethod]
+		public void Deinitialize() {
+			directOutput.Deinitialize();
+			Assert.IsTrue(true);
+		}
+
+	}
 }
