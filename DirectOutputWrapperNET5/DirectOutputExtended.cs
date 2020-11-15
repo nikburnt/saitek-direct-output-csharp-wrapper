1d3677e2b0cb5c800c9dc9a0c8597f4e98ea87ab
Lukas Josai <lukas.josai@student.tuke.sk>
2020-11-15 17:25:01 +0100

    Switched to .NET5 (still windows only)

---
+++ DirectOutputWrapperNET5/DirectOutputExtended.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DirectOutputWrapperNET5
+{
+    public enum Leds
+    {
+        FireIllumination,
+        FireARed,
+        FireAGreen,
+        FireBRed,
+        FireBGreen,
+        FireDRed,
+        FireDGreen,
+        FireERed,
+        FireEGreen,
+        Toggle12Red,
+        Toggle12Green,
+        Toggle34Red,
+        Toggle34Green,
+        Toggle56Red,
+        Toggle56Green,
+        POV2Red,
+        POV2Green,
+        ClutchIRed,
+        ClutchIGreen,
+        ThrottleIllumination
+    }
+
+    public enum LedState
+    {
+        Off,
+        On
+    }
+
+    public enum MFDLines
+    {
+        FirstLine,
+        SecondLine,
+        ThirdLine
+    }
+
+    /// <summary>
+    /// An extended high level api for DirectOutput
+    /// </summary>
+    public class DirectOutputExtended : DirectOutput
+    {
+        /// <summary>
+        /// Overload of SetLed that accepts enumerators
+        /// </summary>
+        /// <param name="device"></param>
+        /// <param name="page"></param>
+        /// <param name="index"></param>
+        /// <param name="value"></param>
+        public void SetLed(IntPtr device, int page, Leds index, LedState value)
+        {
+            SetLed(device, page, (int) index, (int) value);
+        }
+
+        public void SetString(IntPtr device, int page, MFDLines index, string text)
+        {
+            SetString(device, page, (int) index, text);
+        }
+
+        public List<IntPtr> ListDevices()
+        {
+            List<IntPtr> devices = new List<IntPtr>();
+            Enumerate((device, target) =>
+            {
+                devices.Add(device);
+                return target;
+            });
+
+            return devices;
+        }
+    }
+}
---
+++ DirectOutputWrapperNET5/Page.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using DirectOutputWrapperNET5;
+
+namespace DirectOutputCSharpWrapper
+{
+    public class Page
+    {
+        private string _firstLine = "";
+        private string _secondLine = "";
+        private string _thirdLine = "";
+        private DirectOutputExtended _context;
+        private int _pageNum { get; set; }
+        private readonly IntPtr _parentDevice;
+
+        public Page(IntPtr parentDevice, int pageNum, DirectOutputExtended context)
+        {
+            _context = context;
+            _pageNum = pageNum;
+            _parentDevice = parentDevice;
+        }
+
+        public string FirstLine
+        {
+            get => _firstLine;
+            set => _firstLine = value;
+        }
+
+        public string SecondLine
+        {
+            get => _secondLine;
+            set => _secondLine = value;
+        }
+
+        public string ThirdLine
+        {
+            get => _thirdLine;
+            set => _thirdLine = value;
+        }
+
+        /// <summary>
+        /// Writes all string onto MFD, use only on an active page!
+        /// </summary>
+        public void Refresh()
+        {
+            _context.SetString(_parentDevice, _pageNum, MFDLines.FirstLine, _firstLine);
+            _context.SetString(_parentDevice, _pageNum, MFDLines.SecondLine, _secondLine);
+            _context.SetString(_parentDevice, _pageNum, MFDLines.ThirdLine, _thirdLine);
+        }
+    }
+}
